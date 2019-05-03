cdb0ff2bee2d2ca56271613e51760ab8dc77fed3
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-05-02 20:42:00 -0500

    tt

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,21 +1,21 @@
 node{
-  properties([pipelineTriggers([pollSCM('* * * * *')])])
+  properties([parameters([string(defaultValue: 'dev', description: 'IP address', name: 'my_env', trim: true)]), pipelineTriggers([pollSCM('* * * * *')])])
     stage("Pull Repo"){
       git 'git@github.com:Nurjan87/cool_website.git'
     }
     stage("Install Web_Server"){
-      sh "ssh ec2-user@3.19.27.36 sudo yum install httpd -y"
+      sh "ssh ec2-user@${my_env} sudo yum install httpd -y"
     }
 
     stage("Copy Index File"){
-      sh "scp index.html ec2-user@3.19.27.36:/tmp/"
+      sh "scp index.html ec2-user@${my_env}/tmp/"
     }
 
     stage("Move Index File"){
-      sh "ssh ec2-user@3.19.27.36 sudo mv /tmp/index.html /var/www/html/"
+      sh "ssh ec2-user@${my_env} sudo mv /tmp/index.html /var/www/html/"
     }
 
     stage("Restart Apache"){
-      sh "systemctl restart httpd && systemctl enable httpd"
+      sh "ssh ec2-user@${my_env} sudo systemctl restart httpd && systemctl enable httpd"
     }
 }
